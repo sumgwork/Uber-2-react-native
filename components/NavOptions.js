c51692f96ccfd2ca1a7c2c0d351047f342c4a11c
Sumit Govil <sumg.work@gmail.com>
2022-08-02 17:42:17 +1000

    navigation options

---
+++ components/NavOptions.js
@@ -0,0 +1,72 @@
+import {
+  FlatList,
+  Image,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native'
+import React from 'react'
+import tw from 'tailwind-react-native-classnames'
+import { Icon } from 'react-native-elements'
+
+const data = [
+  {
+    id: '123',
+    title: 'Get a ride',
+    image: 'https://links.papareact.com/3pn',
+    screen: 'MapScreen',
+  },
+  {
+    id: '124',
+    title: 'Order food',
+    image: 'https://links.papareact.com/28w',
+    screen: 'EatsScreen',
+  },
+]
+
+const NavOptions = () => {
+  return (
+    <View>
+      <FlatList
+        horizontal
+        data={data}
+        renderItem={({ item }) => (
+          <TouchableOpacity style={tw`p-4 bg-gray-200 m-2`}>
+            <View style={styles.button}>
+              <Image
+                style={styles.image}
+                source={{
+                  uri: item.image,
+                }}
+              />
+              <Text style={tw`text-lg font-semibold mt-2`}>{item.title}</Text>
+              <Icon
+                color="white"
+                name="arrowright"
+                style={tw`p-2 bg-black mt-4 rounded-full w-10`}
+                type="antdesign"
+              />
+            </View>
+          </TouchableOpacity>
+        )}
+        keyExtractor={(item) => item.id}
+      />
+    </View>
+  )
+}
+
+export default NavOptions
+
+const styles = StyleSheet.create({
+  image: {
+    width: 120,
+    height: 120,
+    resizeMode: 'contain',
+  },
+  button: {
+    display: 'flex',
+    alignItems: 'center',
+    width: 120,
+  },
+})
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,26 +1,30 @@
-import React, { Component } from 'react'
-import { Text, SafeAreaView, View, Image } from 'react-native'
+import React from 'react'
+import { Text, SafeAreaView, View, Image, StyleSheet } from 'react-native'
 import tw from 'tailwind-react-native-classnames'
+import NavOptions from '../components/NavOptions'
 
-export class HomeScreen extends Component {
-  render() {
-    return (
-      <SafeAreaView style={tw`bg-white h-full`}>
-        <View style={tw`p-5`}>
-          <Image
-            source={{
-              uri: 'https://links.papareact.com/gzs',
-            }}
-            style={{
-              width: 100,
-              height: 100,
-              resizeMode: 'contain',
-            }}
-          />
-        </View>
-      </SafeAreaView>
-    )
-  }
+const HomeScreen = () => {
+  return (
+    <SafeAreaView style={tw`bg-white h-full`}>
+      <View style={tw`p-5`}>
+        <Image
+          source={{
+            uri: 'https://links.papareact.com/gzs',
+          }}
+          style={styles.image}
+        />
+        <NavOptions />
+      </View>
+    </SafeAreaView>
+  )
 }
 
 export default HomeScreen
+
+const styles = StyleSheet.create({
+  image: {
+    width: 100,
+    height: 100,
+    resizeMode: 'contain',
+  },
+})
