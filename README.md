90f7e527afe22bf80f4fba74fa7729e34f98e94d
Sumit Govil <sumg.work@gmail.com>
2022-08-03 14:19:09 +1000

    Google Places API

---
+++ README.md
@@ -0,0 +1,13 @@
+# Uber 2 app
+
+Uses React Native, Expo Cli, Tailwind CSS, React Native Stack Navigation
+
+## Dependencies
+
+This app makes use of following Google Cloud APIs:
+
+- Directions API
+- Places API (for searching by name or address)
+- Distance matrix API (for calculations)
+
+Generate an API Key and store it into .env file as GOOGLE_MAPS_API_KEY
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,8 +2,14 @@ import React from 'react'
 import { Text, SafeAreaView, View, Image, StyleSheet } from 'react-native'
 import tw from 'tailwind-react-native-classnames'
 import NavOptions from '../components/NavOptions'
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete'
+import { GOOGLE_MAPS_API_KEY } from '@env'
+import { useDispatch } from 'react-redux'
+import { setDestination, setOrigin } from '../slices/navSlice'
 
 const HomeScreen = () => {
+  const dispatch = useDispatch()
+
   return (
     <SafeAreaView style={tw`bg-white h-full`}>
       <View style={tw`p-5`}>
@@ -12,6 +18,31 @@ const HomeScreen = () => {
             uri: 'https://links.papareact.com/gzs',
           }}
           style={styles.image}
+        />
+        <GooglePlacesAutocomplete
+          nearbyPlacesAPI="GooglePlacesSearch"
+          debounce={300}
+          placeholder="Where from"
+          onPress={(data, details = null) => {
+            // 'details' is provided when fetchDetails = true
+            dispatch(
+              setOrigin({
+                location: details.geometry.location,
+                description: data.description,
+              }),
+            )
+
+            dispatch(setDestination(null))
+          }}
+          fetchDetails={true}
+          returnKeyType="search"
+          minLength={2}
+          enablePoweredByContainer={false}
+          query={{
+            key: GOOGLE_MAPS_API_KEY,
+            language: 'en',
+          }}
+          styles={styles.autocomplete}
         />
         <NavOptions />
       </View>
@@ -26,5 +57,13 @@ const styles = StyleSheet.create({
     width: 100,
     height: 100,
     resizeMode: 'contain',
+  },
+  autocomplete: {
+    container: {
+      flex: 0,
+    },
+    textInput: {
+      fontSize: 16,
+    },
   },
 })
